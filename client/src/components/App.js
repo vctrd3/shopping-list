83a7261d5a4387146e700f27a93619609b7b4bfb
Victor <vctrd3@gmail.com>
2020-06-30 12:19:39 -0400

    added react frontend  navbar

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './AppNavbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App;
